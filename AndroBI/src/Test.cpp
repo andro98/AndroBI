33340530032e8e24aea729f2fb2ae396fd7ea891
Andrew <andrewaacodo12@gmail.com>
2020-02-05 13:32:46 +0200

    Setup Project

---
+++ AndroBI/src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include<stdio.h>
+
+namespace AndroBI {
+
+	void Print()
+	{
+		printf("Welcome to AndroBI Engine");
+	}
+}
---
+++ Sandbox/Application.cpp
@@ -0,0 +1,8 @@
+namespace AndroBI {
+	__declspec(dllexport) void Print();
+}
+
+void main()
+{
+	AndroBI::Print();
+}
