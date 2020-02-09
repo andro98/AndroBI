aea5cc82d9e04be6a7aaa46a8e07802c65b04031
Andrew <andrewaacodo12@gmail.com>
2020-02-09 21:33:26 +0200

    Premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+solution "AndroBI"
+	architecture "x86_64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "AndroBI"
+	location "AndroBI"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ABI_PLATFORM_WINDOWS",
+			"ABI_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir.. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ABI_DEBUG"
+		symbols	"On"
+		
+	filter "configurations:Release"
+		defines "ABI_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "ABI_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"AndroBI/vendor/spdlog/include",
+		"AndroBI/src"
+	}
+
+	links
+	{
+		"AndroBI"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ABI_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "ABI_DEBUG"
+		symbols	"On"
+		
+	filter "configurations:Release"
+		defines "ABI_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "ABI_DIST"
+		optimize "On"
